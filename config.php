087451420da300898f8411a2ed1786e67d1d4cde
FXB <fxb@google.com>
2025-04-11 11:09:10 +0000

    Changes to be committed: 	modified:   config.php

--- config.php
+++ config.php
@@ -1,16 +1,22 @@
 <?php
-// Configurazione del database
-$db_host = 'localhost';
-$db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+// Database configuration with ENV variables. Set default values as well 
+$db_host = getenv('DB_HOST') ?: '34.38.137.113';
+$db_name = getenv('DB_NAME') ?: 'image_catalog';
+$db_user = getenv('DB_USER') ?: 'appmod-phpapp-user';
+$db_pass = getenv('DB_PASS') ?: 'genericpassword';
 
 try {
+	//echo "host=$db_host";
+	//echo "name=$db_name";
+	//echo "user=$db_user";
+	//echo "pass=$db_pass";
+
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Errore di connessione: " . $e->getMessage());
+    die("connection error " . $e->getMessage());
 }
 
 session_start();
 ?>
+
